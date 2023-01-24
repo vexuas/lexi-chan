d1366cb1d956b6d2aab7e2d899ef0f3ee72038c9
vexuas <rodriguezgab1601@gmail.com>
2023-01-25 04:09:43 +0800

    Update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Oh wow a cool editor!</p>
+        <div>Just imagine an editor here</div>
       </header>
     </div>
   );
