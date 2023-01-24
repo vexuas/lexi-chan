9452c90d963704a78b333600af8636ed213eaa64
vexuas <rodriguezgab1601@gmail.com>
2023-01-25 04:50:01 +0800

    Create base functions

--- src/app/components/EditorContainer/index.tsx
+++ src/app/components/EditorContainer/index.tsx
@@ -1,10 +1,26 @@
 import styles from './EditorContainer.module.scss';
+import { $getRoot, $getSelection, EditorState } from 'lexical';
 
 export default function EditorContainer() {
+  const theme = {};
+  const onChange = (editorState: EditorState) => {
+    editorState.read(() => {
+      const root = $getRoot();
+      const selection = $getSelection();
+      console.log(root, selection);
+    });
+  };
+  const onError = (error: any) => {
+    console.error(error);
+  };
+  const initialConfig = {
+    namespace: 'Lexi-chan',
+    theme,
+    onError,
+  };
   return (
     <div className={styles.Container}>
       <p>Oh wow a cool editor!</p>
-      <div>Just imagine an editor here</div>
     </div>
   );
 }
