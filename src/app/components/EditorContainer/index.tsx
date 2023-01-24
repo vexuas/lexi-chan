c66bf25af8db9ca4a077e7d7af20db5ce40d258b
vexuas <rodriguezgab1601@gmail.com>
2023-01-25 05:58:45 +0800

    Add comments

--- src/app/components/EditorContainer/index.tsx
+++ src/app/components/EditorContainer/index.tsx
@@ -7,20 +7,38 @@ import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
 import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
 
+//Definitely check out this first and play with the demo: https://lexical.dev/docs/getting-started/react
 export default function EditorContainer() {
+  /**
+   * Theme styling goes here
+   * Basically this adds optional classes to elements used inside the editor content
+   * Full list found in typing in EditorThemeClasses under LexicalEditor.d.ts
+   * More reading here too: https://lexical.dev/docs/getting-started/theming
+   */
   const theme: EditorThemeClasses = {
     paragraph: styles.Paragraph,
   };
+  /**
+   * Custom handler for when content changes in the editor
+   * You'd want to pass this in the OnChangePlugin which will call this whenever the editor changes
+   * Interesting that building the editor requires you to use their built-in plugins and then add your interactions after
+   */
   const onChange = (editorState: EditorState) => {
+    //Read the contents of the EditorState here
     editorState.read(() => {
       const root = $getRoot();
       const selection = $getSelection();
       console.log(root, selection);
     });
   };
+  //Error handling during lexical updates; not really sure what to do with this for now
   const onError = (error: any) => {
     console.error(error);
   };
+  /**
+   * Config necessary in creating a new EditorState
+   * Full list found in LexicalComposer.d.ts
+   */
   const initialConfig: InitialConfigType = {
     namespace: 'Lexi-chan',
     theme,
