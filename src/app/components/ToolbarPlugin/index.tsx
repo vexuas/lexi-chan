e8cc303b390f177283cb1663748af27711a1d95d
vexuas <rodriguezgab1601@gmail.com>
2023-01-27 02:23:23 +0800

    Add main text format buttons

--- src/app/components/ToolbarPlugin/index.tsx
+++ src/app/components/ToolbarPlugin/index.tsx
@@ -9,7 +9,32 @@ export function ToolbarPlugin() {
 
   return (
     <div className={styles.Container}>
-      <button onClick={() => activeEditor.dispatchCommand(FORMAT_TEXT_COMMAND, 'bold')}>B</button>
+      <div className={styles.Tools}>
+        <button
+          className={styles.ToolButton}
+          onClick={() => activeEditor.dispatchCommand(FORMAT_TEXT_COMMAND, 'bold')}
+        >
+          B
+        </button>
+        <button
+          className={styles.ToolButton}
+          onClick={() => activeEditor.dispatchCommand(FORMAT_TEXT_COMMAND, 'italic')}
+        >
+          I
+        </button>
+        <button
+          className={styles.ToolButton}
+          onClick={() => activeEditor.dispatchCommand(FORMAT_TEXT_COMMAND, 'strikethrough')}
+        >
+          S
+        </button>
+        <button
+          className={styles.ToolButton}
+          onClick={() => activeEditor.dispatchCommand(FORMAT_TEXT_COMMAND, 'underline')}
+        >
+          U
+        </button>
+      </div>
     </div>
   );
 }
