b5d440845ac63ecd15bddfa71b45c088d8dd4503
vexuas <rodriguezgab1601@gmail.com>
2023-01-27 04:33:11 +0800

    Add comments

--- src/app/components/ToolbarPlugin/index.tsx
+++ src/app/components/ToolbarPlugin/index.tsx
@@ -9,6 +9,14 @@ import {
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { useCallback, useEffect, useState } from 'react';
 
+/**
+ * Plugins are essentially react components that you can use inside the LexicalComposer wrapper
+ * It's pretty neat since the composer uses Context, we can get the editor state by just calling a native function (editor instance is passed through all the children)
+ * There are native plugins in lexical already but majority of use cases would require us creating custom plugins
+ * I haven't fully grasped everything that's happening yet but I'm slowing understanding all the moving parts
+ * When you're finally done with the plugin, you can just place it inside the EditorContainer under LexicalComposer
+ * @returns
+ */
 export function ToolbarPlugin() {
   const [editor] = useLexicalComposerContext();
   const [activeEditor, setActiveEditor] = useState(editor);
@@ -17,6 +25,11 @@ export function ToolbarPlugin() {
   const [isUnderline, setIsUnderline] = useState(false);
   const [isStrikethrough, setIsStrikethrough] = useState(false);
 
+  /**
+   * Updates the styling of the toolbar
+   * Currently only controls if the text format buttons are active
+   * Probably would get more complicated with more tool actions
+   **/
   const updateToolbar = useCallback(() => {
     const selection = $getSelection() as RangeSelection;
     setIsBold(selection.hasFormat('bold'));
@@ -25,11 +38,18 @@ export function ToolbarPlugin() {
     setIsStrikethrough(selection.hasFormat('strikethrough'));
   }, [activeEditor]);
 
+  /**
+   * Commands lets you register event listeners like `key_enter_command` and contextually react to them wherever & however you'd like
+   * This is pretty powerful as you can control pretty much anything that happens in the dom and act accordingly
+   * There are already a handful of existing commands you can register under LexicalCommands.d.ts but you can also create custom ones
+   * I figure this is the concept that you'll use to do the all fancy editor interactions
+   * Only caveat is that there's not really a detailed explanation of what the existing commands do; guess it's somewhat self-explanatory but still you'd have to test them to be sure
+   * Below listens for any changes in the selection (highlight) of the editor and updates the state of the toolbar buttons
+   */
   useEffect(() => {
     return editor.registerCommand(
       SELECTION_CHANGE_COMMAND,
       (_payload, newEditor) => {
-        console.log('hello');
         updateToolbar();
         setActiveEditor(newEditor);
         return false;
@@ -38,6 +58,12 @@ export function ToolbarPlugin() {
     );
   }, [editor, updateToolbar]);
 
+  /**
+   * Listeners are a mechanism that lets the Editor instance inform the user when a certain operation has occured
+   * Different from commands as they listen for user events and this listens for when the editor has done an event
+   * Again you'd probably use this in tandem with commands for all the fancy interactions
+   * Below listens for any updates to the DOM and updates the toolbar state if it does
+   */
   useEffect(() => {
     return activeEditor.registerUpdateListener(({ editorState }) => {
       editorState.read(() => {
@@ -47,6 +73,7 @@ export function ToolbarPlugin() {
   }, [activeEditor, updateToolbar]);
   return (
     <div className={styles.Container}>
+      {/* Commands can be dispatched anywhere as long as you have access to the editor state */}
       <div className={styles.Tools}>
         <button
           className={`${styles.ToolButton} ${isBold ? styles.Active : ''}`}
